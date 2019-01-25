42016f0ed1343198d8d741f9151c380010bae1d3
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 19:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ class BoardMember {
       return 'No, I must disagree';
     }
     
-    BoardMember.prototype.approve = function(){
+    approve(){
       return 'You can do that!';
-    };
+    }
     
-    BoardMember.prototype.doCharity = function(){
+    doCharity(){
       return "I like to help people.";
-    };
+    }
     
   
-    BoardMember.prototype.releasePressStatement = function(){
+    releasePressStatement(){
       return "You will see great things from Scuber.";
-    };
+    }
   
-    BoardMember.prototype.sayHi = function(){
+    sayHi(){
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
-    };
+    }
 }
